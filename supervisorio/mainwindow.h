7e58af59344117befa3ad320d60dc081c299cca7
Alexander Silva Barbosa <alexander.lmx@outlook.com>
2018-10-07 09:38:10 -0300

    Novo design

--- supervisorio/mainwindow.h
+++ supervisorio/mainwindow.h
@@ -14,12 +14,6 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
-private slots:
-    void on_checkBox_clicked(bool checked);
-
-private slots:
-    void on_pushButton_clicked();
     
 private:
     Ui::MainWindow *ui;
